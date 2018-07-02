c38bc237f73c44232c7cfc9b788010b622b70c7f
Amine Zaghdoudi <az@make.org>
2018-07-02 15:23:11 +0200

    feat(transverse): add licence header and file

--- controllers/configuration.js
+++ controllers/configuration.js
@@ -1,3 +1,23 @@
+/*
+ *
+ * Make.org Front Runner
+ * Copyright (C) 2018 Make.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 const request = require('request-promise');
 const conf = require('../helpers/configuration.js');
 
@@ -7,4 +27,3 @@ exports.config = function() {
   return request(`${apiUrl}/configurations/front`)
     .then(response => JSON.parse(response));
 };
-
