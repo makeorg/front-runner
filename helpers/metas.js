3f88f2e067bccfd0a2847b8f40db22696019b8bc
Amine Zaghdoudi <az@make.org>
2018-09-20 15:43:40 +0200

    feat(operation): add aines meta + fix title fetch

--- helpers/metas.js
+++ helpers/metas.js
@@ -54,6 +54,8 @@ function getOperationImage(operationSlug) {
       return 'https://uploads-ssl.webflow.com/59833d390a24e50001b873d8/5b83cb202b7a399c42b56d3d_meta%20mve.jpg';
     case 'culture':
       return 'https://uploads-ssl.webflow.com/59833d390a24e50001b873d8/5b83cb2046483e731b5f21fe_meta%20culture.jpg';
+    case 'aines':
+      return 'https://uploads-ssl.webflow.com/59833d390a24e50001b873d8/5ba39fc6fafb147685dd9b4c_meta%20aines.jpg';
     default:
       return defaultMetas.picture;
   }
@@ -64,6 +66,10 @@ function getOperationDescription(operationSlug) {
     case 'culture':
       return 'Vous avez un avis sur le sujet ? Alors comme des milliers de citoyens participez à la consultation ' +
              'nationale Make.org : proposez vos idées, réagissez à celles des autres ! Les meilleures seront transformées en actions';
+    case 'aines':
+      return 'Make.org lance avec le ministère des Solidarités et de la Santé une vaste consultation citoyenne en ligne autour de la question :' +
+      '\'Comment mieux prendre soin de nos aînés ?\'. Les milliers de propositions citoyennes recueillies vont nous permettre de bâtir le 1er ' +
+      'grand Plan d\'actions citoyen en faveur des Aînés.';
     default:
       return sequenceMetas.description;
   }
