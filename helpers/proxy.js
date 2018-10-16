146814f002fd91f79daa235f6a07ae8670b9e9aa
Colin Salmon-Legagneur <salmonl.colin@gmail.com>
2018-10-16 16:01:04 +0200

    fix(proxy): secure cookies
    Closes #17

--- helpers/proxy.js
+++ helpers/proxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 const conf = require('./configuration.js');
 
 function relayResponseHeaders(proxyRes, req, res) {
-  if (!req.connection.encrypted && proxyRes.headers['set-cookie']) {
+  if (!conf.proxyApiUrl.startsWith('https://') && proxyRes.headers['set-cookie']) {
     const cookies = proxyRes.headers['set-cookie']
       .map(cookie => cookie.replace(/; secure/gi, ''));
     proxyRes.headers['set-cookie'] = cookies;
